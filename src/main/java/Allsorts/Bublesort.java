0aacc2744747eeb24a14a377b1d0201c1d7a4859
AlexKing3222 <megalex2009@yandex.ru>
2016-10-19 19:12:01 +0400

    Готовый метод сортировки на ветке Buble sort

--- src/main/java/Allsorts/Bublesort.java
+++ src/main/java/Allsorts/Bublesort.java
@@ -26,6 +26,8 @@ int arr[] = new int[(int) (Math.random() * 100)];
 	    for(int i = arr.length-1 ; i > 0 ; i--){
 	        for(int j = 0 ; j < i ; j++){
 	        	
+	        	/* Sravnivaet elementy poparno, esli oni imeut nepravil'niy poryadok, to menyaet mestami.
+	        	 */
 	        	
 	            if( arr[j] > arr[j+1] ){
 	                int tmp = arr[j];
